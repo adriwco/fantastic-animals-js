760fc273254b3161e5fea8ec0bfebb2657148dcb
adriwco <adriwco@gmail.com>
2023-01-23 04:20:03 -0300

    refatoração > exibir > faq & add export + import > ddRules (type="module")

---
+++ js/ddRules.js
@@ -0,0 +1,38 @@
+export let ddRules = {
+  0: [
+    "Na natureza, a expectativa de vida típica de uma raposa é de um a três anos, embora os indivíduos possam viver até dez anos.",
+    "Raposas são mamíferos da família Canidae, assim como lobos e cachorros. As raposas têm o crânio achatado, orelhas triangulares verticais, focinho pontiagudo e ligeiramente arrebitado e uma cauda longa e espessa.",
+    "Em termos de proporções, as raposas são mais alongadas e têm pernas mais curtas do que as outras duas espécies. A cauda é a mais longa em relação ao corpo e a mais espessa, devido à sua densa pelagem. Por outro lado, os lobos são os mais robustos, largos e musculosos entre esses canídeos.",
+    "Implementando práticas de conservação que ajudarão a minimizar a fragmentação da floresta e limitar as atividades que poderiam perturbar as tocas das raposas.",
+  ], // RAPOSA
+  1: [
+    "No que se refere à reprodução, é um animal que vive até 15 anos, e que atinge maturidade sexual já no primeiro ano de vida. Sua gestação dura em torno de 45 dias, e pode originar de dois a três filhotes.",
+    "Os esquilos pertencem a uma grande família de mamíferos roedores, de pequeno e médio porte, conhecida como Sciuridae.",
+    "Seu comprimento varia entre 16 e 30 cm, enquanto o esquilo de árvore (um exemplo de esquilo) é maior e não tem listras e possui comprimento entre 38 e 52 cm. Todos eles têm pelo curto e orelhas arredondadas e diminutas.",
+    "Geralmente, eles cuidam da própria higiene, utilizando a saliva e as patas, em um processo similar ao dos gatos. Porém, se é preciso oferecer ao esquilo-da-mongólia cuidados do tipo, o melhor é optar por opções de banho seco. Até porque, acostumado com desertos e locais áridos, o gerbil prefere se limpar com areia.",
+  ], // ESQUILO
+  2: [
+    "O urso-pardo vive de 25 a 30 anos (máximo conhecido em estado selvagem é de 34 anos e em cativeiro é de 47 anos). Medem de 1,70 m a 2,50 metros de altura, e cerca de 3 metros em pé.",
+    "O urso-pardo é um animal solitário, sabe conviver pacificamente quando existe abundância de alimento. Durante o inverno, ele busca covas e cavernas para entrar em estado de letargia, que pode durar até sete meses.",
+    "O urso pardo é o mais variável em tamanho dentre todos os Ursídeos vivos. Tamanho e peso variam consideravelmente de acordo com a subespécie, ou sazonalmente, em vista de que quando saem da hibernação na primavera estão relativamente magros, devido a falta de forrageamento durante a letargia, ou quando estão na época de acúmulo de gordura, para se preparar para a hibernação.",
+    "Implementando práticas de conservação que ajudarão a minimizar a fragmentação da floresta e limitar as atividades que poderiam perturbar.",
+  ], // URSO
+  3: [
+    "Os lobos são animais que vivem, em média, de cinco a seis anos na natureza. Entretanto, esses animais podem viver até cerca de 13 anos. Em cativeiro, os lobos apresentam expectativa de vida de 15 anos.",
+    "Eles são muito dependentes das mães no período logo após o nascimento. Até os 45 dias de idade, eles receberão comida regurgitada. Após essa fase, receberão carne dos membros da alcateia. Eles só serão capazes de iniciar a caça com o grupo quando completarem cerca de dez meses de idade.",
+    "O termo lobo é, geralmente, utilizado para se referir ao lobo-cinzento. Entretanto, outros animais são também conhecidos como lobos, sendo esse o caso do lobo-guará e do lobo-vermelho.",
+    "Implementando práticas de conservação que ajudarão a minimizar a fragmentação da floresta e limitar as atividades que poderiam perturbar.",
+  ], // LOBO
+  4: [
+    "Quando se falam de macacos é preciso considerar que existem cerca de 300 espécies de primatas espalhadas pelo mundo. A esperança média de vida do chimpanzé, mesmo que inferior à idade média que vive um ser humano, não deixa de ser longa quando comparada a outros animais. Em geral, um chimpanzé poderá chegar tranquilamente aos 50 anos de idade.",
+    "Macaco é um termo de origem africana (provavelmente do banto makako) utilizado como designação comum a todas as espécies de símios ou primatas antropoides. É aplicada restritivamente no Brasil aos cebídeos (ou macacos do Novo Mundo) em geral. No sentido estrito, 'macaco' refere-se às espécies de primatas pertencentes ao género Macaca.",
+    "Os macacos variam em tamanho desde o sagui-pigmeu, que pode ser tão pequeno quanto 117 mm ( 4+5 ⁄ 8  pol) com 172 mm ( 6+3 ⁄ 4  pol) de cauda e pouco mais de 100 g ( 3+1 ⁄ 2  oz) em peso, para o mandril macho, quase 1 m (3 pés 3 pol) de comprimento e pesando até 36 kg (79 lb). Alguns são arbóreos (vivendo em árvores), enquanto outros vivem na savana ; as dietas diferem entre as várias espécies, mas podem conter qualquer um dos seguintes alimentos: frutas, folhas, sementes, nozes, flores, ovos e pequenos animais (incluindo insetos e aranhas).",
+    "Implementando práticas de conservação que ajudarão a minimizar a fragmentação da floresta e limitar as atividades que poderiam perturbar.",
+  ], // MACACO
+  5: [
+    "Para o maior predador africano, tal como para as suas presas, a vida tende a ser curta. Com sorte, um macho adulto pode atingir a provecta idade de 12 anos em ambiente selvagem. As fêmeas podem viver mais, chegando por vezes aos 19 anos.",
+    "O leão é um grande felino e vive em grupos, os quais apresentam uma hierarquia bastante definida. Enquanto as fêmeas caçam, os machos protegem o grupo.",
+    "Segundo maior felino do mundo, o leão perde em tamanho apenas para o tigre. Os leões apresentam de 1,37 a 2 m de tamanho (corpo e cabeça), e sua cauda pode atingir até 100 cm de comprimento. O peso varia entre 120 e 190 quilos, mas vale destacar que já foram descritos indivíduos com mais de 250 kg. O maior leão já encontrado pesava 272 kg.",
+    "Implementando práticas de conservação que ajudarão a minimizar a fragmentação da floresta e limitar as atividades que poderiam perturbar.",
+  ], // LEÃO
+};
