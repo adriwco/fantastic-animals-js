203774d77e1e9088fe8cbb838581d9567706a39d
Adriano Weverton <adriwco@gmail.com>
2023-06-01 17:38:03 -0300

    add function initTooltip

---
+++ js/modules/tooltip.js
@@ -0,0 +1,43 @@
+export default function initTooltip() {
+  const tooltip = document.querySelectorAll("[data-tooltip]");
+
+  const onMouseLeave = {
+    handleEvent() {
+      this.tooltipBox.remove();
+      this.element.removeEventListener("mouseleave", onMouseLeave);
+    },
+  };
+
+  const onMouseMove = {
+    handleEvent(event) {
+      this.tooltipBox.style.top = event.pageY + 20 + "px";
+      this.tooltipBox.style.left = event.pageX + 20 + "px";
+      this.removeEventListener("mousemove", onMouseMove);
+    },
+  };
+
+  function onMouseOver(event) {
+    const tooltipBox = criarTooltipBox(this);
+
+    onMouseLeave.tooltipBox = tooltipBox;
+    onMouseLeave.element = this;
+    this.addEventListener("mouseleave", onMouseLeave);
+
+    onMouseMove.tooltipBox = tooltipBox;
+    this.addEventListener("mousemove", onMouseMove);
+  }
+
+  function criarTooltipBox(element) {
+    const tooltipBox = document.createElement("div");
+    const text = element.getAttribute("aria-label");
+    tooltipBox.classList.add("tooltip");
+    tooltipBox.innerText = text;
+    document.body.appendChild(tooltipBox);
+
+    return tooltipBox;
+  }
+
+  tooltip.forEach((item) => {
+    item.addEventListener("mouseover", onMouseOver);
+  });
+}
--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ import initAccordion from "./modules/accordion.js";
 import initScrollSuave from "./modules/scroll-suave.js";
 import initAnimacaoScroll from "./modules/scroll-animacao.js";
 import initModal from "./modules/modal.js";
+import initTooltip from "./modules/tooltip.js";
 
 initTabNav();
 initAccordion();
 initScrollSuave();
 initAnimacaoScroll();
 initModal();
+initTooltip();
